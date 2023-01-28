c2d60e924ef61980c0e76dccc5592aa67254804f
ThisGuyCodes1106 <79881489+ThisGuyCodes1106@users.noreply.github.com>
2023-01-28 19:33:41 +0000

    initial commit

---
+++ restaurant-app/data.js
@@ -0,0 +1,26 @@
+export const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        id: 0,
+        price: 14,
+        emoji: "🍕",
+        image: "/images/pizza.jpg"
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1,
+        image: "/images/burger.jpg",
+    },
+        {
+        name: "Sushi",
+        ingredients: ["rice", "salmon", "cucumber"],
+        price: 20,
+        emoji: "🍣",
+        id: 2,
+        image: "/images/sushi.jpg",
+    }
+]
